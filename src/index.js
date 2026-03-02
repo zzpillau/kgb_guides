884064b60a7d6756c884f86aba541d24e103fdb2
zzpillau <zzpillau@gmail.com>
2026-03-02 12:15:12 +0200

    chore: initialize Vite + React

--- src/index.js
+++
@@ -1,4 +0,0 @@
-const body = document.body
-const h1 = document.createElement('h1')
-h1.textContent = "HELLO, script!"
-body.append(h1)
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+/** biome-ignore-all assist/source/organizeImports: <explanation> */
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
