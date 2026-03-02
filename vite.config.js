70323f874d71470f703c23f30248dcb5f97ead5b
zzpillau <zzpillau@gmail.com>
2026-03-02 14:08:57 +0200

    chore: add workflows

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-/** biome-ignore-all assist/source/organizeImports: <explanation> */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/kgb_guides/",
   plugins: [react()],
 })
