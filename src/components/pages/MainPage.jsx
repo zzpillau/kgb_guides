8850b196b1dc960ee3f17d47ab391fb188a4b9cc
zzpillau <zzpillau@gmail.com>
2026-03-04 18:18:12 +0200

    style: config basic styles

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,19 +1,16 @@
 import logo from '/logo.png'
-import '../../styles/MainPage.scss'
 
 const MainPage = () => {
-  console.log('mnmn')
 
   return (
     <>
       <div>
         <a href="https://vk.com" target="_blank" rel="noopener">
-          <img src={logo} className="logo react" alt="React logo" />
+          <img src={logo} className="logo" alt="logo" />
         </a>
       </div>
       <h1>Клуб Гидов Балтики</h1>
-      <div className="card"></div>
-      <p className="read-the-docs">
+      <p>
         Начинаем работу над сайтом КГБ 2 марта 2026
       </p>
     </>
