3ba948f47f5d396399dddb3d846e9101cbdde3e8
zzpillau <zzpillau@gmail.com>
2026-03-05 20:56:50 +0200

    feat: add about section layout

--- src/components/layout/sections/About.jsx
+++ src/components/layout/sections/About.jsx
@@ -1,8 +1,39 @@
+import { lazy, Suspense } from 'react'
+import { useTranslation } from 'react-i18next'
+import guides from '../../cards/GuidesCards/guides.js'
+
+const GuideCard = lazy(() => import('../../cards/GuidesCards/GuideCard.jsx'))
+
 const About = () => {
+  const { t } = useTranslation()
+
   return (
-    <section>
-      <div>
-        <p>About</p>
+    <section className="section about">
+      <div className="wrapper">
+        <div className="about__container">
+          <div className="about__content flex-column">
+            <div className="about__titles flex-column">
+              <h3 className="about__title">{t('titles.about_title')}</h3>
+              <h2 className="about__subtitle">{t('titles.about_subtitle')}</h2>
+              <p className="about__text">{t('texts.about_text')}</p>
+            </div>
+            <div className="about__cards flex-row">
+              <Suspense
+                fallback={<div className="cards-loading">{t('loading')}</div>}
+              >
+                {guides.map((guide) => (
+                  <GuideCard
+                    key={guide.id}
+                    src={guide.src}
+                    name={guide.name}
+                    specs={guide.specs}
+                    desc={guide.desc}
+                  />
+                ))}
+              </Suspense>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   )
